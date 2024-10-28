67fb6358bc6e82d568a4a8b497deb70c5c5eaf93
Sara M <sara_murillo82221@elpoli.edu.co>
2024-10-28 17:00:33 -0500

    HU03, 04, 05 Logins
    Validaciones del login

--- public/JS/login.js
+++ public/JS/login.js
@@ -1,14 +1,38 @@
+const p=document.getElementById("warnings");
+const s=document.getElementById("valid")
+
 document.getElementById('loginForm').addEventListener('submit', async function(event) {
     event.preventDefault();
 
-    const emailInput = this.querySelector('input[type="email"]').value;
+    const emailInput = document.getElementById('email').value;
     const passwordInput = this.querySelector('input[type="password"]').value;
-    console.log(emailInput, passwordInput)
 
-    if (passwordInput.trim() === '') {
-        alert('Password cant be empty.');
-        passwordInput.focus();
-        event.preventDefault();
+    let warnings = ""
+    let send=false
+    p.innerHTML = ""
+
+    if(!emailInput || !passwordInput ){
+        warnings+='All fields are required <br>';
+        send=true
+    }   
+    else{
+
+        let regexUsername = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+    
+        if (!regexUsername.test(emailInput)) {
+            warnings+='The username must be an email address <br>';
+            send=true
+        }
+    
+        if(passwordInput.length < 8 || passwordInput.length > 15){
+            warnings+='The password must contain between 8 and 15 characters <br>';
+            send=true
+        }
+    }
+
+    if(send){
+        p.innerHTML =warnings
+        return;
     }
 
     await fetch('/login/', {
@@ -21,27 +45,33 @@ document.getElementById('loginForm').addEventListener('submit', async function(e
             password: passwordInput
         })
     })
-    .then(response => {
-        if (!response.ok) {
-            throw new Error('Error en la solicitud');
-        }
-        return response.json();
-    })
+    .then(response => response.json())
     .then(data => {
-        console.log('Usuario logueado: ', data);
-        console.log(data.Role)
-        if (data.Role === 'Admin'){
-            window.location.href = 'http://localhost:3000/HTML/landingAdmin.html'
-            
-        } else if (data.Role === 'Vet'){
-            window.location.href = 'http://localhost:3000/HTML/landingVet.html'
-
-        } else if (data.Role === 'Pet owner'){
-            window.location.href = 'http://localhost:3000/HTML/landingOwner.html'
-            // localStorage.setItem('ID', data.ID);
+        if (!data.error) {
+            s.innerHTML = data.message;
+            if (data.Role === 'Admin'){
+                window.location.href = 'http://localhost:3000/HTML/landingAdmin.html'
+                
+            } else if (data.Role === 'Vet'){
+                window.location.href = 'http://localhost:3000/HTML/landingVet.html'
+    
+            } else if (data.Role === 'Pet owner'){
+                window.location.href = 'http://localhost:3000/HTML/landingOwner.html'
+                // localStorage.setItem('ID', data.ID);
+            }
+            setTimeout(() => {
+                s.innerHTML = ""; 
+                p.innerHTML = ""; 
+            }, 3000);
+            document.getElementById('loginForm').reset();
+        } else {
+            p.innerHTML = data.error; 
         }
     })
+        
     .catch(error => {
         console.error('Error:', error);
+        p.innerHTML = 'Error processing your request. Please try again.';
     });
+
 });
