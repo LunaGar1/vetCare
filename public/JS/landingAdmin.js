8ff625e06eeac59de37c71a04e6ad1ac1be35ec0
LunaGarcia <luna_penagos82221@elpoli.edu.co>
2024-12-20 00:49:51 -0500

    Admin menu
    Updating the administrator menu

--- public/JS/landingAdmin.js
+++ public/JS/landingAdmin.js
@@ -6,6 +6,10 @@ document.getElementById('profile').addEventListener('click', function() {
     window.location.href = 'http://localhost:3000/user/profile';
 });
 
+document.getElementById('medicines').addEventListener('click', function() {
+    window.location.href = 'http://localhost:3000/medicines/showMedicines';
+});
+
 document.getElementById('logout').addEventListener('click', function() {
     sessionStorage.clear();
     localStorage.clear();
